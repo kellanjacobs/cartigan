7e6e5e9bd3baed930b0134ac1fa2f9f49045468c
GitHub Action <action@github.com>
2022-06-14 05:38:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/kellanjacobs/cartigan/MyBundle.kt
+++ src/main/kotlin/com/github/kellanjacobs/cartigan/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kellanjacobs.cartigan
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kellanjacobs/cartigan/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kellanjacobs/cartigan/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kellanjacobs.cartigan.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kellanjacobs.cartigan.MyBundle
 
 class MyApplicationService {
 
